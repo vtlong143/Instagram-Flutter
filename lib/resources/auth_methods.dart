ee8041f98d80938dd4ce5bc5794e26cc023b4881
Vu Thanh Long <sepdra143@gmail.com>
2022-03-28 01:33:11 +0700

    add functions

--- lib/resources/auth_methods.dart
+++ lib/resources/auth_methods.dart
@@ -75,4 +75,8 @@ class AuthMethods {
     }
     return res;
   }
+
+  Future<void> signOut() async {
+    await _auth.signOut();
+  }
 }
--- lib/resources/firestore_methods.dart
+++ lib/resources/firestore_methods.dart
@@ -1,7 +1,6 @@
 import 'dart:typed_data';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/material.dart';
 import 'package:instagram_flutter/resources/storage_methods.dart';
 import 'package:uuid/uuid.dart';
 
@@ -87,6 +86,32 @@ class FirestoreMethods {
   Future<void> deletePost(String postId) async {
     try {
       await _firestore.collection('posts').doc(postId).delete();
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+  Future<void> followUser(String uid, String followId) async {
+    try {
+      DocumentSnapshot snap =
+          await _firestore.collection('users').doc(uid).get();
+      List following = (snap.data()! as dynamic)['following'];
+
+      if (following.contains(followId)) {
+        await _firestore.collection('users').doc(followId).update({
+          'followers': FieldValue.arrayRemove([uid])
+        });
+        await _firestore.collection('users').doc(uid).update({
+          'following': FieldValue.arrayRemove([followId])
+        });
+      } else {
+        await _firestore.collection('users').doc(followId).update({
+          'followers': FieldValue.arrayUnion([uid])
+        });
+        await _firestore.collection('users').doc(uid).update({
+          'following': FieldValue.arrayUnion([followId])
+        });
+      }
     } catch (e) {
       print(e.toString());
     }
